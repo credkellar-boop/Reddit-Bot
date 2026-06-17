cd8dbbdc426e8de8045d29f1cd4a9ff38b00079a
Darion Kellar <credkellar@gmail.com>
2026-06-16 20:26:38 -0700

    Add configuration for Reddit credentials

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Reddit Credentials
+REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
+REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
+REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
+REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
+REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
